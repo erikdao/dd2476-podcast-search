a9487caf280592e31c439c27f28b79e382120b37
erikdao <cuong@neuraltalks.io>
2021-03-24 08:38:45 +0100

    Update: code formatting for SQL script

--- scripts/create_database.sql
+++ scripts/create_database.sql
@@ -1,46 +1,43 @@
-create table if not exists metadata
-(
-    show_uri                varchar(50) not null,
-    show_name               varchar(255),
-    show_description        text,
-    publisher               varchar(255),
-    language                varchar(50),
-    rss_link                varchar(255),
-    episode_uri             varchar(255),
-    episode_name            varchar(255),
-    episode_description     text,
-    duration                numeric(15, 10),
-    show_filename_prefix    varchar(50),
-    episode_filename_prefix varchar(50),
-    id                      serial      not null
-    constraint metadata_pkey primary key,
-    constraint metadata_show_uri_episode_uri_key unique (show_uri, episode_uri)
+CREATE TABLE IF NOT EXISTS metadata (
+	show_uri varchar(50) NOT NULL,
+	show_name varchar(255),
+	show_description text,
+	publisher varchar(255),
+	LANGUAGE varchar
+(50),
+	rss_link varchar(255),
+	episode_uri varchar(255),
+	episode_name varchar(255),
+	episode_description text,
+	duration numeric(15, 10),
+	show_filename_prefix varchar(50),
+	episode_filename_prefix varchar(50),
+	id serial NOT NULL CONSTRAINT metadata_pkey PRIMARY KEY,
+	CONSTRAINT metadata_show_uri_episode_uri_key UNIQUE (show_uri, episode_uri)
 );
 
-alter table metadata owner to postgres;
+ALTER TABLE metadata OWNER TO postgres;
 
-create table if not exists shows
-(
-    show_uri             varchar(50) not null constraint shows_pkey primary key,
-    show_name            varchar(255),
-    show_description     text,
-    publisher            varchar(255),
-    language             varchar(50),
-    rss_link             varchar(255),
-    show_filename_prefix varchar(50)
+CREATE TABLE IF NOT EXISTS shows (
+	show_uri varchar(50) NOT NULL CONSTRAINT shows_pkey PRIMARY KEY,
+	show_name varchar(255),
+	show_description text,
+	publisher varchar(255),
+	LANGUAGE varchar
+(50),
+	rss_link varchar(255),
+	show_filename_prefix varchar(50)
 );
 
-alter table shows owner to postgres;
+ALTER TABLE shows OWNER TO postgres;
 
-create table if not exists episodes
-(
-    episode_uri             varchar(50) not null constraint episodes_pkey primary key,
-    episode_name            varchar(255),
-    episode_description     text,
-    duration                numeric(15, 10),
-    episode_filename_prefix varchar(50),
-    show_uri                varchar(50) not null  constraint fk_shows references shows
+CREATE TABLE IF NOT EXISTS episodes (
+	episode_uri varchar(50) NOT NULL CONSTRAINT episodes_pkey PRIMARY KEY,
+	episode_name varchar(255),
+	episode_description text,
+	duration numeric(15, 10),
+	episode_filename_prefix varchar(50),
+	show_uri varchar(50) NOT NULL CONSTRAINT fk_shows REFERENCES shows
 );
 
-alter table episodes owner to postgres;
-
+ALTER TABLE episodes OWNER TO postgres;
