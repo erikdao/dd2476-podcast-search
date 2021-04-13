e5f2fba9e73558020083d5d036811aeaef12d547
erikdao <cuong@neuraltalks.io>
2021-04-13 18:50:15 +0200

    Add: new backend structure with Gradle

---
+++ backend/src/main/java/dd2476/group18/podcastsearch/PodcastSearchApplication.java
@@ -0,0 +1,13 @@
+package dd2476.group18.podcastsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodcastSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodcastSearchApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dd2476/group18/podcastsearch/PodcastSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package dd2476.group18.podcastsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodcastSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
