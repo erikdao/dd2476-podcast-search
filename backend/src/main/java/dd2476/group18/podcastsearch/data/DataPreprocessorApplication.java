45e6c22fdcc20276a9d4fdc434cae555cf30a7e0
erikdao <cuong@neuraltalks.io>
2021-04-16 23:48:43 +0200

    Add: first version for parsing metadata tsv

--- backend/src/main/java/dd2476/group18/podcastsearch/data/DataPreprocessorApplication.java
+++ backend/src/main/java/dd2476/group18/podcastsearch/data/DataPreprocessorApplication.java
@@ -1,5 +1,20 @@
 package dd2476.group18.podcastsearch.data;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+import com.univocity.parsers.common.DataProcessingException;
+import com.univocity.parsers.common.ParsingContext;
+import com.univocity.parsers.common.RowProcessorErrorHandler;
+import com.univocity.parsers.common.processor.BeanListProcessor;
+import com.univocity.parsers.tsv.TsvParser;
+import com.univocity.parsers.tsv.TsvParserSettings;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,15 +22,36 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DataPreprocessorApplication implements CommandLineRunner {
     public static void main(String[] args) {
-        System.out.println("Starting the application");
         SpringApplication.run(DataPreprocessorApplication.class, args);
-        System.out.println("Application finished");
     }
 
     @Override
     public void run(String... args) {
-        for (int i = 0; i < args.length; i++) {
-            System.out.println(args[i]);
+        // As a start, try to read the metadata.tsv file
+        String projectDir = System.getProperty("user.dir");
+        Path tsvPath = Paths.get(projectDir + "/data/podcasts-transcript/metadata.tsv");
+
+        try {
+            BufferedReader reader = Files.newBufferedReader(tsvPath);
+            BeanListProcessor<MetadataBean> rowProcessor = new BeanListProcessor<MetadataBean>(MetadataBean.class);
+            TsvParserSettings settings = new TsvParserSettings();
+            settings.setHeaderExtractionEnabled(true);
+            settings.setProcessor(rowProcessor);
+            settings.setMaxCharsPerColumn(4096 * 3);
+            settings.setProcessorErrorHandler(new RowProcessorErrorHandler(){
+                @Override
+                public void handleError(DataProcessingException error, Object[] inputRow, ParsingContext context) {
+                    System.err.println("Error processing row: " + Arrays.toString(inputRow));
+                    System.err.println("Error details: column '" + error.getColumnName() + "' (index " + error.getColumnIndex() + ") has value '" + inputRow[error.getColumnIndex()] + "'");
+                }
+            });
+
+            TsvParser parser = new TsvParser(settings);
+            parser.parse(reader);
+            List<MetadataBean> metadata = rowProcessor.getBeans();
+            System.out.println("Sample: " + metadata.get(0).getLanguage());
+        } catch (IOException e) {
+            System.err.println("Cannot read file " + tsvPath.toString());
         }
     }
 }
---
+++ backend/src/main/java/dd2476/group18/podcastsearch/data/MetadataBean.java
@@ -0,0 +1,144 @@
+package dd2476.group18.podcastsearch.data;
+
+import com.univocity.parsers.annotations.Parsed;
+import com.univocity.parsers.annotations.Replace;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class MetadataBean {
+    @Parsed(field = "show_uri")
+    private String showUri;
+    
+    @Parsed(field = "show_name")
+    private String showName;
+
+    @Parsed(field = "show_description")
+    private String showDescription;
+
+    @Parsed(field = "publisher")
+    private String publisher;
+
+    @Parsed(field = "language")
+    private String language;
+
+    @Parsed(field = "rss_link")
+    private String rssLink;
+
+    @Parsed(field = "show_filename_prefix")
+    private String showFileNamePrefix;
+
+    @Parsed(field = "episode_uri")
+    private String episodeUri;
+
+    @Parsed(field = "episode_name")
+    private String episodeName;
+
+    @Parsed(field = "episode_description")
+    private String episodeDescription;
+
+    @Parsed(field = "duration")
+    private Double duration;
+
+    @Parsed(field = "episode_filename_prefix")
+    private String episodeFilenamePrefix;
+
+    public String getShowUri() {
+        return showUri;
+    }
+
+    public void setShowUri(String showUri) {
+        this.showUri = showUri;
+    }
+
+    public String getShowName() {
+        return showName;
+    }
+
+    public void setShowName(String showName) {
+        this.showName = showName;
+    }
+
+    public String getShowDescription() {
+        return showDescription;
+    }
+
+    public void setShowDescription(String showDescription) {
+        this.showDescription = showDescription;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    @Parsed
+    public void setLanguage(String language) {
+        this.language = language.replace("['", "").replace("']", "");
+    }
+
+    public String getRssLink() {
+        return rssLink;
+    }
+
+    public void setRssLink(String rssLink) {
+        this.rssLink = rssLink;
+    }
+
+    public String getShowFileNamePrefix() {
+        return showFileNamePrefix;
+    }
+
+    public void setShowFileNamePrefix(String showFileNamePrefix) {
+        this.showFileNamePrefix = showFileNamePrefix;
+    }
+
+    public String getEpisodeUri() {
+        return episodeUri;
+    }
+
+    public void setEpisodeUri(String episodeUri) {
+        this.episodeUri = episodeUri;
+    }
+
+    public String getEpisodeName() {
+        return episodeName;
+    }
+
+    public void setEpisodeName(String episodeName) {
+        this.episodeName = episodeName;
+    }
+
+    public String getEpisodeDescription() {
+        return episodeDescription;
+    }
+
+    public void setEpisodeDescription(String episodeDescription) {
+        this.episodeDescription = episodeDescription;
+    }
+
+    public Double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Double duration) {
+        this.duration = duration;
+    }
+
+    public String getEpisodeFilenamePrefix() {
+        return episodeFilenamePrefix;
+    }
+
+    public void setEpisodeFilenamePrefix(String episodeFilenamePrefix) {
+        this.episodeFilenamePrefix = episodeFilenamePrefix;
+    }
+}
