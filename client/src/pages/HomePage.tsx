47b4473d1e0accfa7fde20c485015fb12bc9e080
erikdao <cuong@neuraltalks.io>
2021-04-23 23:10:31 +0200

    Add: basic setup including tailwindcss

---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div className="min-h-screen bg-black">
+      <h1 className="text-xl text-white">Hello Podcast search</h1>
+    </div>
+  );
+}
+
+export default HomePage;
